acdf1f530d84dd3a39b9aeebd45ebce9d284cc84
Paul B-H <51964537+PaulB-H@users.noreply.github.com>
2019-10-29 02:53:33 -0400

    search button now works and clears current and forecast divs before running search based on text in search box

--- script.js
+++ script.js
@@ -1,60 +1,66 @@
 const apiKey = "0765d126b0f6a7eb158764d733ae5823";
 let cityName = "Toronto";
 
-$(document).ready( function () {
+$(document).ready(function () {
 
 
-function currentWeather() {
-    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${apiKey}`;
+    function currentWeather() {
+        let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${apiKey}`;
 
-    $.get(`${queryURL}`, function (response) {
-        console.log(response);
-        var d = new Date(response.dt * 1000);
-        $("#currentName").html(`${response.name}`);
-        $("#currentDetails").html(`
-            <h3>${d.getDay()}/${d.getMonth() + 1}/${d.getFullYear()}</h3>
-            <p style="margin-top: -10px;">DD/MM/YY</p>
-            <p>Current Weather:</p>
-            <p>Temp: ${response.main.temp}&deg;c</p>
-            <p>Humidex: ${response.main.humidity}%</p>
-            <p>Wind: ${response.wind.speed} m/s</p>
+        $.get(`${queryURL}`, function (response) {
+            console.log(response);
+            var d = new Date(response.dt * 1000);
+            var iconCode = (response.weather[0].icon);
+            var iconUrl = `"http://openweathermap.org/img/wn/${iconCode}@2x.png"`
+            $("#currentCard").append(`
+            <div class="card-body">
+            <img src=${iconUrl} class="card-img-top" alt="...">
+            <h1 class="card-title">${response.name}</h1>
+            <div id="currentDetails" class="card-text">
+                <h3>${d.getDay()}/${d.getMonth() + 1}/${d.getFullYear()}</h3>
+                <p style="margin-top: -10px;">DD/MM/YY</p>
+                <p>Current Weather:</p>
+                <p>Temp: ${response.main.temp}&deg;c</p>
+                <p>Humidex: ${response.main.humidity}%</p>
+                <p>Wind: ${response.wind.speed} m/s</p>
+            </div>
         `);
 
-        // console.log(response.wind.deg); // Could calculate to N,S,E,W
+            // console.log(response.wind.deg); // Could calculate to N,S,E,W
 
-        var lat = response.coord.lat;
-        var lon = response.coord.lon;
-        var uvIndexUrl = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon} `;
-        $.get(`${uvIndexUrl}`, function (response) {
-            console.log(`UV Value was: ${response.value}`);
-            $("#currentDetails").append(`
+            var lat = response.coord.lat;
+            var lon = response.coord.lon;
+            var uvIndexUrl = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon} `;
+            $.get(`${uvIndexUrl}`, function (response) {
+                console.log(`UV Value was: ${response.value}`);
+                $("#currentDetails").append(`
             <p id="uvIndex">UV: ${response.value}</p>
         `);
+            });
+
         });
+    }
 
-    });
-}
+    currentWeather();
 
-currentWeather();
+    function forecastWeather() {
+        let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&APPID=${apiKey}`;
 
-function forecastWeather() {
-    let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&APPID=${apiKey}`;
+        $.get(`${queryURL}`, function (response) {
+            console.log(response);
 
-    $.get(`${queryURL}`, function (response) {
-        console.log(response);
-
-        for (var i = 0, l = `${response.list.length}`; i < l; i++) {
-            var obj = response.list[i];
-             if (response.list[i].dt_txt.includes("12:00:00")) {
-                var iconCode = (response.list[i].weather[0].icon);
-                var iconUrl = `"http://openweathermap.org/img/wn/${iconCode}@2x.png"`
-                var d = new Date(response.list[i].dt * 1000);
-                var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-                var dayNum = d.getDay();
-                var dayName = days[dayNum];
-                $("#forecastDeck").append(` 
+            for (var i = 0, l = `${response.list.length}`; i < l; i++) {
+                var obj = response.list[i];
+                if (response.list[i].dt_txt.includes("12:00:00")) {
+                    var iconCode = (response.list[i].weather[0].icon);
+                    var iconUrl = `"http://openweathermap.org/img/wn/${iconCode}@2x.png"`
+                    var d = new Date(response.list[i].dt * 1000);
+                    var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+                    var dayNum = d.getDay();
+                    var dayName = days[dayNum];
+                    $("#forecastDeck").append(` 
                     <div class="card">
-                        <img src=${iconUrl} class=""  card-img-top" alt="...">
+                        <img src=${iconUrl} class="card-img-top" alt="...">
                         <div class="card-body">
                             <h3 class="card-title">${dayName}</h3>
                             <p class="card-text">
@@ -65,17 +71,22 @@ function forecastWeather() {
                         </div>
                     </div>
                  `);
-             }
-        }
-    })
-};
+                }
+            }
+        })
+    };
 
-forecastWeather();
+    forecastWeather();
 
-$(searchButton).click(function(){
-    console.log("Search Button Clicked");
-    cityName = searchBox.value;
-    console.log(cityName)
-});
+    $(searchButton).click(function () {
+        console.log("Search Button Clicked");
+        cityName = searchBox.value;
+        console.log(cityName);
+        $("#currentCard").empty();
+        currentWeather(cityName);
+        $("#forecastDeck").empty();
+        forecastWeather(cityName);
+        
+    });
 
 })
