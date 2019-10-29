89e177675cdc626a196e861b65571580accf3019
Paul B-H <51964537+PaulB-H@users.noreply.github.com>
2019-10-29 02:24:39 -0400

    Got search button console-logging

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const apiKey = "0765d126b0f6a7eb158764d733ae5823";
 let cityName = "Toronto";
 
+$(document).ready( function () {
+
+
 function currentWeather() {
     let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${apiKey}`;
 
     $.get(`${queryURL}`, function (response) {
         console.log(response);
         var d = new Date(response.dt * 1000);
-        console.log(`D-${d.getDay()}/M-${d.getMonth() + 1}/Y-${d.getFullYear()}`);
-        console.log(response.name);
         $("#currentName").html(`${response.name}`);
         $("#currentDetails").html(`
             <h3>${d.getDay()}/${d.getMonth() + 1}/${d.getFullYear()}</h3>
@@ -18,16 +19,14 @@ function currentWeather() {
             <p>Humidex: ${response.main.humidity}%</p>
             <p>Wind: ${response.wind.speed} m/s</p>
         `);
-        console.log(response.weather[0].icon);
-        console.log(response.main.temp);
-        console.log(response.main.humidity);
+
         // console.log(response.wind.deg); // Could calculate to N,S,E,W
-        console.log(response.wind.speed); // meters per second
+
         var lat = response.coord.lat;
         var lon = response.coord.lon;
         var uvIndexUrl = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon} `;
         $.get(`${uvIndexUrl}`, function (response) {
-            console.log(response.value);
+            console.log(`UV Value was: ${response.value}`);
             $("#currentDetails").append(`
             <p id="uvIndex">UV: ${response.value}</p>
         `);
@@ -44,23 +43,15 @@ function forecastWeather() {
     $.get(`${queryURL}`, function (response) {
         console.log(response);
 
-        // GOAL for each list.dt_txt contains 12:00:00 print data
-        // SUCCESS IT WORKS!!
-
-        console.log(response.list.length);
-
         for (var i = 0, l = `${response.list.length}`; i < l; i++) {
             var obj = response.list[i];
              if (response.list[i].dt_txt.includes("12:00:00")) {
-                console.log("One 12:00:00 Forecast");
-                console.log(response.list[i].weather[0].icon);
                 var iconCode = (response.list[i].weather[0].icon);
                 var iconUrl = `"http://openweathermap.org/img/wn/${iconCode}@2x.png"`
                 var d = new Date(response.list[i].dt * 1000);
                 var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
                 var dayNum = d.getDay();
                 var dayName = days[dayNum];
-                console.log(` Day Got: ${d.toUTCString()}` );
                 $("#forecastDeck").append(` 
                     <div class="card">
                         <img src=${iconUrl} class=""  card-img-top" alt="...">
@@ -80,3 +71,9 @@ function forecastWeather() {
 };
 
 forecastWeather();
+
+$(searchButton).click(function(){
+    console.log("Search Button Clicked");
+});
+
+})
