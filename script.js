252ec08346af40784b23ba04efc04840f077bf27
Paul B-H <51964537+PaulB-H@users.noreply.github.com>
2019-10-28 21:01:26 -0400

    Added section in currentWeather to call and return UV Index

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 const apiKey = "0765d126b0f6a7eb158764d733ae5823";
 let cityName = "Toronto";
 
-
 function currentWeather() {
     let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${apiKey}`;
 
     $.get(`${queryURL}`, function(response) {
         console.log(response);
-        console.log(response.dt*1000); 
+        console.log(response.dt*1000); // *1000 because its UNIX Time
         console.log(response.name);
         console.log(response.main.humidity);
         console.log(response.main.temp);
-        console.log(response.wind.deg);
-        console.log(response.wind.speed); // in meters / second   
+        // console.log(response.wind.deg); // Could calculate to N,S,E,W
+        console.log(response.wind.speed); // meters/second
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
+
+        var uvIndexUrl =` http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon} `;
+        $.get(`${uvIndexUrl}`, function(response) {
+            console.log(response.value);
+        });
+        
     });
 }
 
